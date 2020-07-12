ac8eabd20012f6788ddd7e210c5bd221071c4cc6
Mikael Finstad <finstaden@gmail.com>
2020-07-12 21:58:25 +0200

    Updates

---
+++ precache-manifest.572a6dcd74681f7529a208302cbedf1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1935e0f58339f5f0fd544073442ddc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2aebcacda31db2e2eea6",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "bf2a2efa2f7513c9845e",
+    "url": "./static/js/2.4495988b.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "./static/js/2.4495988b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aebcacda31db2e2eea6",
+    "url": "./static/js/main.2b146339.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.2b146339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64ff77d8da108a5866b4",
+    "url": "./static/js/runtime-main.429a2470.js"
+  }
+]);
