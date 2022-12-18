1dd835705e5195b152431a9fe4c7c7e0052f9c91
Mikael Finstad <finstaden@gmail.com>
2022-12-18 23:12:26 +0800

    disable treeshake for lottie-web

--- example/vite.config.js
+++ example/vite.config.js
@@ -7,4 +7,25 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '',
+  build: {
+    rollupOptions: {
+      plugins: [
+        // https://github.com/airbnb/lottie-web/issues/2599
+        {
+          name: 'disable-treeshake',
+          transform(code, id) {
+            if (/node_modules[/\\]lottie-web/.test(id)) {
+              // Disable tree shake for lottie-web module
+              return {
+                code,
+                map: null,
+                moduleSideEffects: 'no-treeshake',
+              };
+            }
+            return null;
+          },
+        },
+      ],
+    },
+  },
 });
